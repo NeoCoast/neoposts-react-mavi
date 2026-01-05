3f8116c255439c0e55910ebb8eed536f33c60c67
VictoriaRivao <victoria.rivao@neocoast.com>
2026-01-05 15:03:57 -0300

    done setting up redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './index.scss';
+import { store } from './services/store';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,10 @@
+/// <reference types="vite/client" />
 import { createApi } from '@reduxjs/toolkit/query/react';
 import { fetchBaseQuery } from '@reduxjs/toolkit/query';
 
-const apiBaseUrl = process.env.API_URL;
+const apiBaseUrl = import.meta.env.VITE_API_URL;
 if (!apiBaseUrl) {
-  throw new Error('Environment variable API_URL must be defined');
+  throw new Error('Environment variable VITE_API_URL must be defined');
 }
 
 export const api = createApi({
