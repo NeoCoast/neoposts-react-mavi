b52a9b60e4bac1070f0be56e7cab27080aeb21d2
VictoriaRivao <victoria.rivao@neocoast.com>
2026-01-06 14:24:46 -0300

    fixed login error with valid data

--- src/services/api.ts
+++ src/services/api.ts
@@ -68,10 +68,6 @@ export const api = createApi({
         method: 'POST',
         url: 'users/sign_in'
       }),
-      transformResponse: (response, meta) => ({
-        meta,
-        response
-      }),
     }),
   }),
 });
