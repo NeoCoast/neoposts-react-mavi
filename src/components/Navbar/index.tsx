aa7499a843afe904865764dca455c4a2a72b702c
VictoriaRivao <victoria.rivao@neocoast.com>
2026-02-06 15:24:20 -0300

    added link to people icon and posts list

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { RootState, AppDispatch } from '@/services/store';
@@ -15,44 +15,31 @@ import './styles.scss';
 const neoPostIcon = new URL('@/assets/Icons/NeoPost.svg', import.meta.url).href;
 
 const Navbar = () => {
-  const navigate = useNavigate();
-
   const dispatch = useDispatch<AppDispatch>();
   const menuOpen = useSelector((state: RootState) => state.ui.mobileMenuOpen);
 
   const openMenu = () => dispatch(openMobileMenu());
   const closeMenu = () => dispatch(closeMobileMenu());
 
-  const goHome = () => navigate(ROUTES.HOME);
-  const goUsers = () => navigate(ROUTES.USERS);
-
   return (
     <>
       <nav className={`navbar ${menuOpen ? 'navbar--menuOpen' : ''}`}>
         <div className="navbar__left">
-          <Button
-            variant="icon"
-            className="navbar__left-title"
-            aria-label="Go to home"
-            onClick={goHome}
-            title={
-              <>
-                <img className="navbar__left-title-logo" src={neoPostIcon} alt="Neopost" decoding="async" />
-                <span>NEOPOST</span>
-              </>
-            }
-          />
+          <Link to={ROUTES.HOME} className="btn navbar__left-title btn--icon" aria-label="Go to home">
+            <span className="btn__content">
+              <img className="navbar__left-title-logo" src={neoPostIcon} alt="Neopost" decoding="async" />
+              <span>NEOPOST</span>
+            </span>
+          </Link>
         </div>
 
         {!menuOpen && (
           <div className="navbar__icons">
-            <Button
-              variant='icon'
-              className="navbar__icons-iconBtn navbar__icons-peopleBtn"
-              aria-label="People"
-              onClick={goUsers}
-              title={<GoPeople className="navbar__icons-iconBtn-icon" />}
-            />
+            <Link to={ROUTES.USERS} className="navbar__icons-iconBtn navbar__icons-peopleBtn btn btn--icon" aria-label="People">
+              <span className="btn__content">
+                <GoPeople className="navbar__icons-iconBtn-icon" />
+              </span>
+            </Link>
             <Button
               variant='icon'
               className="navbar__icons-hamburger"
--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,5 +1,4 @@
-import { useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router';
 
 import Post from '@/components/Post';
 import AuthorDetails from '@/components/Post/AuthorDetails';
@@ -9,34 +8,20 @@ import type { PostsListProps, PostListItem } from '@/ts/interfaces';
 import './styles.scss';
 
 const PostsList = ({ items }: PostsListProps) => {
-  const navigate = useNavigate();
-
-  const handlePostClick = useCallback((post: PostListItem) => {
-    navigate(`/posts/${post.id}`, { state: { post: post } });
-  }, [navigate]);
 
   return (
     <div className="posts__list">
       {items.map((post) => (
-        <article
-          key={post.id}
-          className="posts__list-item"
-          role="button"
-          tabIndex={0}
-          onClick={() => handlePostClick(post)}
-          onKeyDown={({ key }) => {
-            if (key === 'Enter' || key === ' ') {
-              handlePostClick(post);
-            }
-          }}
-        >
-          <AuthorDetails
-            name={post.author.name}
-            lastName={post.author.lastName}
-            email={post.author.email}
-            profilePhoto={post.author.profilePhoto}
-          />
-          <Post post={post} />
+        <article key={post.id} className="posts__list-item">
+          <Link to={`/posts/${post.id}`} state={{ post }} className="posts__list-item-link">
+            <AuthorDetails
+              name={post.author.name}
+              lastName={post.author.lastName}
+              email={post.author.email}
+              profilePhoto={post.author.profilePhoto}
+            />
+            <Post post={post} />
+          </Link>
         </article>
       ))}
     </div>
